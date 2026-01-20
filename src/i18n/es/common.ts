d39ae127eb77d17ff7862c9e1382df87fc7f876b
sefinsurance <155559377+sefinsurance@users.noreply.github.com>
2026-01-19 21:49:07 -0500

    Add files via upload

--- src/i18n/es/common.ts
+++ src/i18n/es/common.ts
@@ -1,4 +1,5 @@
+
 export const common = {
-  nav:{ home:"Inicio", aca:"ACA", medicare:"Medicare", life:"Vida", medicaid:"Perdiendo Medicaid" },
-  cta:{ qualify:"Ver si califico", switchToEn:"EN" }
+  nav:{ home:"Inicio", aca:"ACA", medicare:"Medicare", life:"Vida" },
+  cta:{ qualify:"Ver si califico" }
 };
