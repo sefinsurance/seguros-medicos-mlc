0560be45fbc1b655eaa51978c14444c3e8073108
sefinsurance <155559377+sefinsurance@users.noreply.github.com>
2026-01-19 21:43:02 -0500

    Create common.ts

---
+++ src/i18n/es/common.ts
@@ -0,0 +1,4 @@
+export const common = {
+  nav:{ home:"Inicio", aca:"ACA", medicare:"Medicare", life:"Vida", medicaid:"Perdiendo Medicaid" },
+  cta:{ qualify:"Ver si califico", switchToEn:"EN" }
+};
