3618d4c01c2b9e8a70b5b39ed1e5fa0fd6ea99a9
sefinsurance <155559377+sefinsurance@users.noreply.github.com>
2026-03-01 22:19:24 -0500

    Add files via upload

---
+++ src/data/coverage.ts
@@ -0,0 +1,41 @@
+export const coverage: Record<string, string[]> = {
+  "florida": [
+    "miami-dade","broward","palm-beach","orange","hillsborough","pinellas","duval","lee","polk","osceola"
+  ],
+  "texas": [
+    "harris","dallas","tarrant","bexar","travis","el-paso","hidalgo","collin","denton","fort-bend"
+  ],
+  "north-carolina": [
+    "mecklenburg","wake","guilford","durham","forsyth","cumberland","union","johnston"
+  ],
+  "south-carolina": [
+    "greenville","richland","charleston","horry","spartanburg","lexington","york"
+  ],
+  "georgia": [
+    "fulton","gwinnett","cobb","dekalb","clayton","chatham"
+  ],
+  "arizona": [
+    "maricopa","pima","pinal","yavapai","mohave"
+  ],
+  "tennessee": [
+    "davidson","shelby","knox","hamilton","rutherford","williamson"
+  ],
+  "ohio": [
+    "cuyahoga","franklin","hamilton","summit","montgomery"
+  ],
+  "indiana": [
+    "marion","lake","allen","hamilton","st-joseph"
+  ],
+  "michigan": [
+    "wayne","oakland","macomb","kent","genesee"
+  ],
+  "missouri": [
+    "st-louis","jackson","st-charles","greene","clay"
+  ],
+  "mississippi": [
+    "hinds","harrison","desoto","lafayette","madison"
+  ],
+  "alabama": [
+    "jefferson","mobile","madison","montgomery","shelby"
+  ]
+};
